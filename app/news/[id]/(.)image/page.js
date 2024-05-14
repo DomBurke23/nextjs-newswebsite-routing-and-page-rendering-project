aa4635cec4e9f80035eecc3d568e767fb9ee2243
dsmith-FootballPools <dominique.smith@footballpools.com>
2024-05-14 15:07:54 +0100

    interceptor route

---
+++ app/news/[id]/(.)image/page.js
@@ -0,0 +1,23 @@
+// define page as it should be shown if the navigation request was intercepted
+// so, if a user tried to visit this specific route whilse already being on the website
+
+import { notFound } from "next/navigation";
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default function InterceptedImagePage({ params }) {
+  const newsItemId = params.id;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsItemId);
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <>
+      <h2>Intercepted!</h2>
+      <div className="fullscreen-image">
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+      </div>
+    </>
+  );
+}
