5108fc5fd4918418ec0ddc8426320d204ec1bd03
dsmith-FootballPools <dominique.smith@footballpools.com>
2024-05-14 15:19:29 +0100

    navigating programmatically

--- app/news/[id]/@modal/(.)image/page.js
+++ app/news/[id]/@modal/(.)image/page.js
@@ -1,10 +1,11 @@
 // define page as it should be shown if the navigation request was intercepted
 // so, if a user tried to visit this specific route whilse already being on the website
-
-import { notFound } from "next/navigation";
+"use client";
+import { notFound, useRouter } from "next/navigation";
 import { DUMMY_NEWS } from "@/dummy-news";
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
   const newsItemId = params.id;
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsItemId);
 
@@ -14,7 +15,7 @@ export default function InterceptedImagePage({ params }) {
 
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
